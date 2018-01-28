a024e89791f9f49e28a87b92f09f7f2a123207a1
Victor <adsvictor@outlook.com>
2018-01-28 20:29:31 -0200

    Organização na estrutura do objeto Twit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'vOuuRLoieMruK0x5732RFeTm8',
+    consumer_secret:      '3OXJ780uTje0jpv84I3c1D3B5w5YPDK3anPNCAJczOv2c9TU62',
+    access_token:         '957723639046078464-uHDNBCtwavkMr9schvRgzZZlO8s1wYP',
+    access_token_secret:  'vpPG0hN1I9w5VJEOYU5CFN3Hf60dcNISTINHruU6iGbGn',
+}
