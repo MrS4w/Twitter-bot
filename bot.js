b3037e556f720333e4e72eb3b515d6eea718b4e2
Victor <adsvictor@outlook.com>
2018-01-28 20:56:47 -0200

    Reconhece quando um tweet é duplicado e mostra seu erro

--- bot.js
+++ bot.js
@@ -1,4 +1,21 @@
 console.log("iniciando!");
+
 var Twit = require('twit');
+
 var config = require('./config');
+
 var T = new Twit(config);
+
+var tweet ={
+  status:'twitting from node.js!'
+}
+
+T.post('statuses/update',tweet,tweeted);
+
+function tweeted(err, data, response) {
+  if(err){
+    console.log("Algo foi errado!");
+  }else {
+    console.log("Funcionou!");
+  }
+}
