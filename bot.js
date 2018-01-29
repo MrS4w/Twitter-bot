f82d40edbd63df763dc441bba6ebb0080146ffc7
Victor <adsvictor@outlook.com>
2018-01-28 22:19:27 -0200

    Tweeta um agradecimento ao seguir

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-console.log("iniciando!");
+console.log("Rodando!");
 
 var Twit = require('twit');
 
@@ -6,22 +6,51 @@ var config = require('./config');
 
 var T = new Twit(config);
 
+
+//Seleciona um usuário stream
+var stream = T.stream('user');
+//Quando alguém me segue
+stream.on('follow',followed);
+
+function followed(eventMsg) {
+  console.log("Mensagem de seguir!");
+  var name = eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  tweetMsg('Olá @'+screenName+' obrigado por me seguir!');
+}
+//Tweetando números aleatórios
 tweetIt();
 setInterval(tweetIt,1000*30);
 
 function tweetIt() {
   var r = Math.floor(Math.random()*9999);
   var tweet ={
-    status:'Tweetando um número aleatório: '+r+'!'
+    status: 'Tweetando um número aleatório: '+r+'!'
   }
 
   T.post('statuses/update',tweet,tweeted);
 
   function tweeted(err, data, response) {
     if(err){
-      console.log("Algo foi errado!");
+      console.log("Algo deu errado no tweet de num!");
     }else {
-      console.log("Funcionou!");
+      console.log("Tweetou um numero!");
+    }
+  }
+}
+//Tweetando mensagens de follow
+function tweetMsg(txt) {
+  var tweet ={
+    status: txt
+  }
+
+  T.post('statuses/update',tweet,tweeted);
+
+  function tweeted(err, data, response) {
+    if(err){
+      console.log("Algo deu errado no follow msg!");
+    }else {
+      console.log("Tweetou msg de follow!");
     }
   }
 }
