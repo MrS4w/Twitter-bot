9604e7b601905d3ab45cc238a759801bee30f5be
Victor <adsvictor@outlook.com>
2018-01-29 00:38:10 -0200

    Atualização dos tokens do Twitter

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     consumer_key:         'vOuuRLoieMruK0x5732RFeTm8',
     consumer_secret:      '3OXJ780uTje0jpv84I3c1D3B5w5YPDK3anPNCAJczOv2c9TU62',
-    access_token:         '957723639046078464-uHDNBCtwavkMr9schvRgzZZlO8s1wYP',
-    access_token_secret:  'vpPG0hN1I9w5VJEOYU5CFN3Hf60dcNISTINHruU6iGbGn',
+    access_token:         '957723639046078464-sOPOP50TZvF1RGE0katScswa7CCEBOE',
+    access_token_secret:  'gxWzJMjFvHOC9dNiCUR2vLYkCqgtcrhXd9P0ZzfeamIB0',
 }
