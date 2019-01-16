27d5c375a22f91c5ad86ef46269e1b61dd44f270
Victor <adsvictor@outlook.com>
2019-01-16 15:41:46 -0200

    Remove semicolons, code not used, changed var to let and identation

--- bot.js
+++ bot.js
@@ -1,58 +1,57 @@
-console.log("Running!");
+console.log("Running!")
 
-var Twit = require('twit');
+let Twit = require('twit')
 
-var config = require('./config');
+let config = require('./config')
 
-var T = new Twit(config);
+let T = new Twit(config)
 
 // Selects a user stream
-var stream = T.stream('user');
+let stream = T.stream('user')
 
 // When someone follows me
-stream.on('follow', followed);
+stream.on('follow', followed)
 
 
 function followed(eventMsg) {
-  console.log("Following message!");
-  var name = eventMsg.source.name;
-  var screenName = eventMsg.source.screen_name;
-  tweetMsg('Hi @' + screenName + ' thanks for following me!');
+  console.log("Following message!")
+  let screenName = eventMsg.source.screen_name
+  tweetMsg('Hi @' + screenName + ' thanks for following me!')
 }
 
 // Tweet random numbers
-tweetIt();
-setInterval(tweetIt, 1000 * 30);
+tweetIt()
+setInterval(tweetIt, 1000 * 30)
 
 function tweetIt() {
-  var r = Math.floor(Math.random() * 9999);
-  var tweet = {
+  let r = Math.floor(Math.random() * 9999)
+  let tweet = {
     status: 'Tweeting a random number: ' + r + '!'
   }
 
-  T.post('statuses/update', tweet, tweeted);
+  T.post('statuses/update', tweet, tweeted)
 
   function tweeted(err, data, response) {
     if (err) {
-      console.log("Something wrong with the random number!");
+      console.log("Something wrong with the random number!")
     } else {
-      console.log("Tweeted a number!");
+      console.log("Tweeted a number!")
     }
   }
 }
 // Tweets thank you message
 function tweetMsg(txt) {
-  var tweet = {
+  let tweet = {
     status: txt
   }
 
-  T.post('statuses/update', tweet, tweeted);
+  T.post('statuses/update', tweet, tweeted)
 
   function tweeted(err, data, response) {
     if (err) {
-      console.log("Something wrong with the thank you message!");
+      console.log("Something wrong with the thank you message!")
     } else {
-      console.log("Tweeted a thank you message!");
+      console.log("Tweeted a thank you message!")
     }
   }
 }
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'vOuuRLoieMruK0x5732RFeTm8',
-    consumer_secret:      '3OXJ780uTje0jpv84I3c1D3B5w5YPDK3anPNCAJczOv2c9TU62',
-    access_token:         '957723639046078464-sOPOP50TZvF1RGE0katScswa7CCEBOE',
-    access_token_secret:  'gxWzJMjFvHOC9dNiCUR2vLYkCqgtcrhXd9P0ZzfeamIB0',
+    consumer_key: 'vOuuRLoieMruK0x5732RFeTm8',
+    consumer_secret: '3OXJ780uTje0jpv84I3c1D3B5w5YPDK3anPNCAJczOv2c9TU62',
+    access_token: '957723639046078464-sOPOP50TZvF1RGE0katScswa7CCEBOE',
+    access_token_secret: 'gxWzJMjFvHOC9dNiCUR2vLYkCqgtcrhXd9P0ZzfeamIB0',
 }
